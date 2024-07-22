902c12a7946e491dec0858290ad4039e85616cbd
YokuTM <alikin.volodia@yandex.ru>
2024-07-22 13:04:53 +0300

    firstcommit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [data, setData] = useState([]);
+  const [index, setIndex] = useState(2);
+
+  useEffect(() => {
+    fetch("https://dummyjson.com/users/" + index)
+      .then((res) => res.json())
+      .then((json) => setData(json));
+  }, []);
+
+  
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Пользователи</h2>
+      <div class="table-responsive small">
+        <table class="table table-striped table-sm table-bordered">
+          <thead>
+            <tr>
+              <th scope="col">ФИО</th>
+              <th scope="col">Возраст</th>
+              <th scope="col">Пол</th>
+              <th scope="col">Номер телефона</th>
+              <th scope="col">Адрес</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>
+                {data.firstName} {data.lastName} {data.maidenName}
+              </td>
+              <td>{data.age}</td>
+              <td>{data.gender}</td>
+              <td>{data.phone}</td>
+              <td>{data.address.city}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
